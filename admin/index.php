2bb242d67787936e6a5838b829a90d6b572024f5
erdalbej <erdbej@gmail.com>
2015-12-20 17:34:56 +0100

    login page fixed lol

--- admin/index.php
+++ admin/index.php
@@ -7,17 +7,22 @@
 	<meta charset="UTF-8">
 	<title>Login</title>
 	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
 </head>
 <body>
 	<section id="login">
 		<form action="">
-			<label for="username">Username:</label>
-			<input type="text" name="username">
-			<br>
-			<label for="password">Password:</label>
-			<input type="text" name="password">
-			<br>
-			<input type="submit" class="login-button">
+			
+					<label for="username">Användarnamn:</label>
+					<input type="text" name="username">
+					<br>
+					<label for="password">Lösenord:</label>
+					<input type="text" name="password">
+					<br>
+					<input type="submit" class="login-button button-primary" value="Logga in">
+			
+
+			
 		</form>
 	</section>
 </body>
