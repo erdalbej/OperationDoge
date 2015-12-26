96b1181c58ca60ebf2f7ca9535da135c28a02c51
Robert Barlin <robert.barlin@gmail.com>
2015-12-26 10:40:40 +0100

    add ContactMessage table and remove latin1 character set

--- dbScript.sql
+++ dbScript.sql
@@ -9,83 +9,92 @@ DROP TABLE IF EXISTS `Participant`;
 DROP TABLE IF EXISTS `DogCourse`;
 DROP TABLE IF EXISTS `Puppy`;
 DROP TABLE IF EXISTS `PuppyLitter`;
+DROP TABLE IF EXISTS `ContactMessage`;
 
 CREATE TABLE Admin (
-    `Username` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `Password` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `Username` varchar(255) NOT NULL,
+    `Password` varchar(255) NOT NULL,
     PRIMARY KEY (`Username`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE News (
-    `Title` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `Title` varchar(255) NOT NULL,
     `DateTime` DATETIME NOT NULL,
-    `NewsText` varchar(255) CHARACTER SET latin1,
-    `NewsImagePath` varchar(255) CHARACTER SET latin1,
+    `NewsText` varchar(255),
+    `NewsImagePath` varchar(255),
     PRIMARY KEY (`Title`,`DateTime`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE ImageGallery (
-    `ImagePath` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `ImageTitle` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `ImagePath` varchar(255) NOT NULL,
+    `ImageTitle` varchar(255) NOT NULL,
     PRIMARY KEY (`ImagePath`, `ImageTitle`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE GuestbookThread (
-    `Title` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `Title` varchar(255) NOT NULL,
     `DateTime` DATETIME NOT NULL,
-    `Username` varchar(255) CHARACTER SET latin1,
-    `Description` varchar(255) CHARACTER SET latin1,
+    `Username` varchar(255),
+    `Description` varchar(255),
     PRIMARY KEY (`Title`, `DateTime`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Post (
-    `Username` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `Username` varchar(255) NOT NULL,
     `DateTime` DATETIME NOT NULL,
-    `PostText` varchar(255) CHARACTER SET latin1,
-    `PostImagePath` varchar(255) CHARACTER SET latin1,
-    `Thread_Title` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `PostText` varchar(255),
+    `PostImagePath` varchar(255),
+    `Thread_Title` varchar(255) NOT NULL,
     `Thread_DateTime` DATETIME,
     PRIMARY KEY (`Username`,`DateTime`),
     FOREIGN KEY  (`Thread_Title`, `Thread_DateTime`) REFERENCES GuestbookThread (`Title`, `DateTime`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE DogCourse (
-    `CourseName` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `CourseTeacher` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `CourseName` varchar(255) NOT NULL,
+    `CourseTeacher` varchar(255) NOT NULL,
     `DateTime` DATETIME NOT NULL,
     `AgeOfDog` int,
-    `Gender` varchar(255) CHARACTER SET latin1,
-    `PriorKnowledge` varchar(255) CHARACTER SET latin1,
-    `CourseText` varchar(255) CHARACTER SET latin1,
+    `Gender` varchar(255),
+    `PriorKnowledge` varchar(255),
+    `CourseText` varchar(255),
     PRIMARY KEY (`CourseName`, `CourseTeacher`, `DateTime`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Participant (
-    `DogName` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `OwnerName` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `DogName` varchar(255) NOT NULL,
+    `OwnerName` varchar(255) NOT NULL,
     `AgeOfDog` int NOT NULL,
-    `Gender` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `ExtraInfo` varchar(255) CHARACTER SET latin1,
-    `DogCourse_CourseName` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `DogCourse_CourseTeacher` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `Gender` varchar(255) NOT NULL,
+    `ExtraInfo` varchar(255),
+    `DogCourse_CourseName` varchar(255) NOT NULL,
+    `DogCourse_CourseTeacher` varchar(255) NOT NULL,
     `DogCourse_DateTime` DATETIME NOT NULL,
     PRIMARY KEY (`DogName`, `OwnerName`),
     FOREIGN KEY (`DogCourse_CourseName`,`DogCourse_CourseTeacher`,`DogCourse_DateTime`) REFERENCES DogCourse (`CourseName`, `CourseTeacher`, `DateTime`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE PuppyLitter (
-    `LitterTitle` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `LitterTitle` varchar(255) NOT NULL,
     `Upcomming` BOOLEAN NOT NULL DEFAULT 1,
-    `LiterInfo` varchar(255) CHARACTER SET latin1,
+    `LiterInfo` varchar(255),
     PRIMARY KEY (`LitterTitle`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Puppy (
-    `DogName` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `Gender` varchar(255) CHARACTER SET latin1 NOT NULL,
-    `Price` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `DogName` varchar(255) NOT NULL,
+    `Gender` varchar(255) NOT NULL,
+    `Price` varchar(255) NOT NULL,
     `Available` BOOLEAN,
-    `PuppyLitter_LitterTitle` varchar(255) CHARACTER SET latin1 NOT NULL,
+    `PuppyLitter_LitterTitle` varchar(255) NOT NULL,
     PRIMARY KEY (`DogName`),
     FOREIGN KEY (`PuppyLitter_LitterTitle`) REFERENCES PuppyLitter (`LitterTitle`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ContactMessage` (
+  `Email` varchar(255) NOT NULL,
+  `Posted` datetime NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Message` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Email`,`Posted`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
