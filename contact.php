cc2482ded08ec503a2b457db73d32e6931643022
Robert Barlin <robert.barlin@gmail.com>
2015-12-26 10:08:40 +0100

    add contact page with info and map

---
+++ contact.php
@@ -0,0 +1,51 @@
+<?php
+include_once 'header.php';
+include_once 'aside.php';
+?>
+<main>
+	<div class="container">
+
+		<div class="row contactRow">
+			<div class="twelve columns">
+				<label>Namn </label>
+				<span>John Doe</span>
+			</div>
+		</div>
+
+		<div class="row contactRow">
+			<div class="twelve columns">
+				<label>Adress </label>
+				<span>Ole Römers väg 6, Lund</span>
+			</div>
+		</div>
+
+		<div class="row contactRow">
+			<div class="twelve columns">
+				<label>Telefon </label>
+				<span><a href="tel:+46555555555">+46 555 555 555</a></span>
+			</div>
+		</div>
+
+		<div class="row contactRow">
+			<div class="twelve columns">
+				<label>Mail </label>
+				<span><a href="mailto:john.doe@mail.com">john.doe@mail.com</a></span>
+				<hr>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="twelve columns">
+				<iframe 
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2247.7612934265917!2d13.211435015825314!3d55.71052090271986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465397b5eef19a89%3A0x50d854d198f468dc!2sOle+R%C3%B6mers+v%C3%A4g+6%2C+223+63+Lund!5e0!3m2!1ssv!2sse!4v1451120672607" 
+					width="600" 
+					height="450" 
+					frameborder="0" 
+					style="border:0" allowfullscreen>
+				</iframe>
+			</div>
+		</div>
+
+	</div>
+</main>
