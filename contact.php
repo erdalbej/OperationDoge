bc30e88c25a8422d16c3eefc557744e864bc238b
Robert Barlin <robert.barlin@gmail.com>
2015-12-26 10:24:21 +0100

    add better map and make it 'reponsive'

--- contact.php
+++ contact.php
@@ -34,18 +34,17 @@ include_once 'aside.php';
 			</div>
 		</div>
 
-
-		<div class="row">
+		<div class="row mapRow">
 			<div class="twelve columns">
 				<iframe 
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2247.7612934265917!2d13.211435015825314!3d55.71052090271986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465397b5eef19a89%3A0x50d854d198f468dc!2sOle+R%C3%B6mers+v%C3%A4g+6%2C+223+63+Lund!5e0!3m2!1ssv!2sse!4v1451120672607" 
 					width="600" 
-					height="450" 
+					height="400" 
 					frameborder="0" 
 					style="border:0" allowfullscreen>
 				</iframe>
 			</div>
+			<label><a href="https://goo.gl/maps/Lhj8XogtHwq">Karta</a></label>
 		</div>
-
 	</div>
 </main>
