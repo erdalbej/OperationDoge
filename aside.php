3c6d625da817ea093b44b6478ec5463b2d0109c6
erdalbej <erdbej@gmail.com>
2015-12-27 19:51:14 +0100

    aside on index is somewhat nice

--- aside.php
+++ aside.php
@@ -1,50 +1,21 @@
 <aside>
 	<span>NYHETER</span>
-
-	<h3>Hundkurser</h3>
-
 	<?php
-	$divOne = query("SELECT CourseName, CourseText FROM DogCourse LIMIT 3");
+	$divOne = query("SELECT NewsTitle, Description, NewsImagePath, NewsLink FROM NewsFeed");
 	$divData = $divOne['data'];
 	foreach($divData as $key => $row){
-		echo '<span id="dogcourses' . $key . '"></span>';
+		echo '<span id="newsfeed' . $key . '"></span>';
 		echo '<div class="sidebar-item">';
-		echo '<span class="course-name">';
-		echo $row['CourseName'];
-		echo '</span>';
-		echo '<p class="course-desc">';
-		echo $row['CourseText'];
-		echo '</p>';
-		echo '<span class="course-more">Läs mer</span>';
+		echo '<div>';
+		echo $row['NewsTitle'];
+		echo '</div>';
+		echo '<img src="uploads/'.$row['NewsImagePath'].'" alt="">';
+		echo '<div>';
+		echo $row['Description'];
+		echo '</div>';
+		echo '<br><a href="'.$row['NewsLink'].'" class="course-more">Läs mer</a>';
+		echo '</div>';
 		echo '<br><br>';
 	}			
 	?>	
-
-	<?php
-	$divOne = query("SELECT CourseName, CourseText FROM PuppyLitter LIMIT 3");
-	$divData = $divOne['data'];
-	foreach($divData as $key => $row){
-		echo '<span id="puppylitter' . $key . '"></span>';
-		echo '<div class="sidebar-item">';
-		echo '<span class="course-name">';
-		echo $row['CourseName'];
-		echo '</span>';
-		echo '<p class="course-desc">';
-		echo $row['CourseText'];
-		echo '</p>';
-		echo '<span class="course-more">Läs mer</span>';
-		echo '<br><br>';
-	}			
-	?>	
-
-	<div class="sidebar-item">
-		<img src="#" alt="">
-		<a href="#">text that will be linked or?</a>
-	</div>
-
-	<div class="sidebar-item">
-		<img src="#" alt="">
-		<a href="#">text that will be linked or? #2</a>
-	</div>
-
 </aside>
