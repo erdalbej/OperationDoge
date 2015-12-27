1e68f48836555ff925ae03e82b60456ac2818c83
Robert Barlin <robert.barlin@gmail.com>
2015-12-28 00:52:24 +0100

    more for school

--- school.php
+++ school.php
@@ -4,19 +4,56 @@ include_once 'aside.php';
 ?>
 
 <?php
-	$result = query("SELECT * FROM ComingCourses");
+	date_default_timezone_set('Europe/Stockholm');
 	
-	if ($result['err'] == null){
-		$courses = $result['data'];
+	if (isset($_POST['submit-regToCourse'])){
+		if (strlen($_POST['dogName']) > 0 &&
+		 	strlen($_POST['ownerName']) > 0 && 
+		 	strlen($_POST['dogAge']) > 0 && 
+		 	strlen($_POST['dogGender']) > 0 &&
+		 	strlen($_POST['courseName']) > 0 &&
+		 	strlen($_POST['courseTeacher']) > 0 &&
+		 	strlen($_POST['courseDate']) > 9){
+
+			$result = query('SELECT * FROM DogCourse WHERE CourseName = :courseName AND CourseTeacher = :courseTeacher AND CourseDate = :courseDate',
+				array(
+					':courseName' => $_POST['courseName'],
+					':courseTeacher' => $_POST['courseTeacher'],
+					':courseDate' => $_POST['courseDate']
+				)
+			);
+
+			if ($result['err'] == null){
+				if (count($result['data']) > 0){
+					$data = $result['data'];
+					$foundCourse = $data[0];
+
+					if ($foundCourse['CourseDate'] <= date('Y-m-d')){
+						$submitError = 'Kursen du försöker registerar dig till har utgått';
+					}
+
+					#if ($foundCourse['AgeOfDog'])
+
+
+
+					if (!isset($submit)){
+
+					} 
+				} else { $submitError = 'Den kurs du försöker registerar dig till finns inte!'; }
+			} else { $submitError = 'Problem med att hitta kursen, prova igen!'; }
+		} else { $submitError = 'Saknar värden, fyll i igen!'; }
 	} else {
-		$coursesError = "Gick inte att läsa kommande kurses";
+		$result = query('SELECT * FROM ComingCourses');
+	
+		if ($result['err'] == null){ $courses = $result['data']; } 
+		else { $coursesError = "Gick inte att läsa kommande kurser"; }
 	}
 ?>
 
 <main>
 	<div class="container">
 		<?php 
-			if (!isset($coursesError)){
+			if (isset($courses)){
 				foreach($courses as $key => $c){
 					echo 
 						'<div class="row">' .
@@ -35,9 +72,12 @@ include_once 'aside.php';
 				}
 			} else {
 				print_r($coursesError);
+				print_r($submitError);
+				print_r($_POST);
+				echo ' <a href="school.php">Gå tillbaks här</a>';
 			}
 		?>
-		<form hidden>
+		<form hidden method="POST">
 			<div class="row">
 				<div class="twelve columns">
 					<h3 id="formHeader"><h3>
@@ -69,10 +109,13 @@ include_once 'aside.php';
 		    <label for="exampleMessage">Extra information</label>
 		    <textarea class="u-full-width" placeholder="Hej! Min hund har problem med..." id="extraInfo" name="extraInfo"></textarea>
 		    <label>
-		      <input disabled type="checkbox" id="reserveSpot">
-		      <span class="label-body">Reservplats</span>
+		      <input style="display:none;" type="checkbox" id="reserveSpot" name="reserveSpot" checked="reserve">
+		      <span hidden class="label-body" id="reserve-spot-label">Reservplats</span>
 		    </label>
 		    <input class="button-primary" type="submit" value="Skicka anmälan" name="submit-regToCourse">
+		    <input style="display:none" type="text" name="courseName" id="formCourseName">
+		    <input style="display:none" type="text" name="courseTeacher" id="formCourseTeacher">
+		    <input style="display:none" type="text" name="courseDate" id="formCourseDate">
 		    <form>
 	</div>		
 </main>
@@ -86,18 +129,25 @@ $(document).ready(function(){
 		var numberOfParticipants = parseInt(numberOfParticipants);
 
 		var name = $(this).parent().find('.courseInfoHeader').html();
-		var time = $(this).parent().find('.course-date').html();
+		name = name.split(' - ');
+		courseName = name[0];
+		courseTeacher = name[1];
+		courseDate = $(this).parent().find('.course-date').html();
+
+		$('#formCourseName').prop('value', courseName);
+		$('#formCourseTeacher').prop('value',courseTeacher);
+		$('#formCourseDate').prop('value',courseDate);
 
 		if (numberOfParticipants >= 10){
 			$('#reserveSpot').prop('checked', true);
+			$('#reserve-spot-label').show();
+		} else {
+			$('#reserve-spot-label').hide();
+			$('#reserveSpot').prop('checked', false);
 		}
 
-		console.log(name);
-		console.log(time);
 
-		$('#formHeader').html(name + ' - ' + time);
-
-		
+		$('#formHeader').html(name[0] + ' - ' + name[1] + ' - ' + courseDate);
 	})
 })
 </script>
