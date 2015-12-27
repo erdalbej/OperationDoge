ea2140e256fb6b4c80225bb0f642884a0dd6e3bb
erdalbej <erdbej@gmail.com>
2015-12-27 15:16:52 +0100

    aside got some fucking progress

--- aside.php
+++ aside.php
@@ -1,4 +1,25 @@
 <aside>
+	<span>NYHETER</span>
+
+	<h3>Hundkurser</h3>
+
+	<?php
+	$divOne = query("SELECT CourseName, CourseText FROM DogCourse LIMIT 3");
+	$divData = $divOne['data'];
+	foreach($divData as $key => $row){
+		echo '<span id="dogcourses' . $key . '"></span>';
+		echo '<div class="sidebar-item">';
+		echo '<span class="course-name">';
+		echo $row['CourseName'];
+		echo '</span>';
+		echo '<p class="course-desc">';
+		echo $row['CourseText'];
+		echo '</p>';
+		echo '<span class="course-more">Läs mer</span>';
+		echo '<br><br>';
+	}			
+	?>	
+
 	<div class="sidebar-item">
 		<img src="#" alt="">
 		<a href="#">text that will be linked or?</a>
