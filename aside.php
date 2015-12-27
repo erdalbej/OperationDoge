33adf03067eda929cb6ff0906c9ec9cad52ea374
erdalbej <erdbej@gmail.com>
2015-12-27 15:25:22 +0100

    aside progress

--- aside.php
+++ aside.php
@@ -20,6 +20,23 @@
 	}			
 	?>	
 
+	<?php
+	$divOne = query("SELECT CourseName, CourseText FROM PuppyLitter LIMIT 3");
+	$divData = $divOne['data'];
+	foreach($divData as $key => $row){
+		echo '<span id="puppylitter' . $key . '"></span>';
+		echo '<div class="sidebar-item">';
+		echo '<span class="course-name">';
+		echo $row['CourseName'];
+		echo '</span>';
+		echo '<p class="course-desc">';
+		echo $row['CourseText'];
+		echo '</p>';
+		echo '<span class="course-more">Läs mer</span>';
+		echo '<br><br>';
+	}			
+	?>	
+
 	<div class="sidebar-item">
 		<img src="#" alt="">
 		<a href="#">text that will be linked or?</a>
