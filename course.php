9e6d7400e331268088bb195648d503e6c3760ede
Robert Barlin <robert.barlin@gmail.com>
2015-12-28 15:00:32 +0100

    course page done

--- course.php
+++ course.php
@@ -24,33 +24,65 @@ if (strlen($courseName) > 0 &&
 		$courses = $courseResult['data'];
 		if (count($courses) > 0){
 			$foundCourse = $courses[0];
-		} else {
-			$courseError = 'Den kursen du letar efter finns inte.';
-		}
 
-		$foundCourse = $courses[0];
+			$participantResult = query('SELECT * FROM Participant WHERE DogCourse_CourseName = :courseName AND DogCourse_CourseTeacher = :courseTeacher AND DogCourse_CourseDate = :courseDate ORDER BY RegisterDate ASC', 
+				array(
+					':courseName' => $courseName,
+					':courseTeacher' => $courseTeacher,
+					':courseDate' => $courseDate
+				)
+			);
 
-
-	} else {
-		$courseError = 'Något gick fel, prova söka efter kursen igen!';
-	}
-
-} else {
-	$courseError =  'Saknar värden för att hitta kursinformation.';
-}
+			if ($participantResult['err'] == null){
+				$participants = $participantResult['data'];
+			} else { $participantError = 'Gick inte att läsa deltagare för kursen, prova igen!'; }
+		} else { $courseError = 'Den kursen du letar efter finns inte.'; }
+	} else { $courseError = 'Något gick fel, prova söka efter kursen igen!'; }
+} else { $courseError =  'Saknar värden för att hitta kursinformation.'; }
 ?>
 <main>
 	<div class="container">
 		<div class="row">
 			<div class="twelve columns">
-				<h1>Kursinformation</h1>
+				<h1 style="text-align: center;">Kursinformation</h1>
 				<hr>
 			</div>
 		</div>
-		<?php
-			echo $courseError;
-			print_r($_GET);
-		?>
+		<div class="row">
+			<div class="twelve columns">
+				<?php
+					if (!isset($courseError)){
+						echo 
+							'<h5>' . $foundCourse['CourseName'] .' - ' . $foundCourse['CourseTeacher'] . '</h5>' .
+							'<p><i>' . $foundCourse['CourseDate'] . '</i><p>';
+
+						if (!isset($participantError)){
+							if (count($participants) > 0){
+								foreach($participants as $key => $p){
+									if (($key+1) > 10){
+										echo '<p><b style="color: red;">Reservdeltagare</b></p>';
+									}
+
+									$tempGender = $p['Gender'];
+
+									if ($tempGender == 'F'){
+										$tempGender = 'Tik';
+									} else if ($tempGender == 'M'){
+										$tempGender = 'Hane';
+									} else {
+										$tempGender = 'Båda';
+									}
+
+									echo '<p>' . ($key+1) . ' - ' . $p['OwnerName'] . ', ' . $p['DogName'] . ', ' . $tempGender . ', ' . $p['AgeOfDog'] . ' år' . '<p>';
+								}
+							} else { echo '<p>Inga deltagare har anmält sig till kursen.</p>'; }
+						} else { echo $participantError; echo $participantResult['err']; }
+					} else { echo $courseError; }
+				?>
+				<hr>
+				<span><a href="school.php">Tillbaka</a></span>
+			</div>
+		</div>
 	</div>		
 </main>
 
