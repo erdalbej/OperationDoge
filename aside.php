cbf9ce3bbc0ad3132cce0a31854bd4a612359e06
erdalbej <erdbej@gmail.com>
2015-12-28 15:25:35 +0100

    aside progress indexview

--- aside.php
+++ aside.php
@@ -1,21 +1,25 @@
 <aside>
-	<span>NYHETER</span>
+	<span class="aside-title">Nyheter</span>
 	<?php
 	$divOne = query("SELECT NewsTitle, Description, NewsImagePath, NewsLink FROM NewsFeed");
 	$divData = $divOne['data'];
 	foreach($divData as $key => $row){
 		echo '<span id="newsfeed' . $key . '"></span>';
 		echo '<div class="sidebar-item">';
-		echo '<div>';
+		echo '<div class="news-title">';
 		echo $row['NewsTitle'];
 		echo '</div>';
-		echo '<img src="uploads/'.$row['NewsImagePath'].'" alt="">';
-		echo '<div>';
+		if($row['NewsImagePath'] !== null){
+			echo '<img src="uploads/'.$row['NewsImagePath'].'" class="newsfeed-img" alt="">';
+		}
+		echo '<div class="news-desc">';
 		echo $row['Description'];
 		echo '</div>';
-		echo '<br><a href="'.$row['NewsLink'].'" class="course-more">Läs mer</a>';
+		if($row['NewsLink'] !== null){
+			echo '<br><a href="'.$row['NewsLink'].'" class="news-more">Läs mer</a>';
+		}
+		echo '<hr class="newsfeed-sep">';
 		echo '</div>';
-		echo '<br><br>';
 	}			
 	?>	
 </aside>
