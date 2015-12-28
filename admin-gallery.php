ea2ebe56b28a66fdf3ef6f461c7b4185bc816d6c
toaap <mrtoap@hotmail.com>
2015-12-28 14:05:50 +0100

    admin fixer

--- admin-gallery.php
+++ admin-gallery.php
@@ -24,7 +24,7 @@ if(isset($_POST['submit_image'])){
 					//2mb
 					if($file_size < 2097152){	
 						$file_name_new = uniqid('', true) . '.' . $file_ext;
-						$file_destination = '../uploads/' . $file_name_new;
+						$file_destination = 'uploads/' . $file_name_new;
 
 						if(move_uploaded_file($file_tmp, $file_destination)){
 							$result = nonQuery("INSERT INTO ImageGallery (`ImagePath`,`ImageTitle`) VALUES (:image_path,:image_title)", array(":image_path" => $file_name_new, ":image_title" => $image_title));
@@ -78,7 +78,7 @@ if(isset($_POST['submit_image'])){
 						foreach($divData as $key => $row){
 							echo '<tr id="gallery' . $key . '">';
 							echo '<td class="galleryImageTd">';
-							echo '<img src="../uploads/'.$row['ImagePath'].'" width="100" height="100" alt="">';
+							echo '<img src="uploads/'.$row['ImagePath'].'" width="100" height="100" alt="">';
 							echo '</td>';
 							echo '<td class="galleryTitleTd">';
 							echo $row['ImageTitle'];
--- admin-header.php
+++ admin-header.php
@@ -27,12 +27,13 @@
             <nav>
                 <ul>
                     <li>Hantera sidorna nedan:</li>
-                    <li><a href="manage-home.php">Hem</a></li>
-                    <li><a href="#">Annonser</a></li>
+                    <li><a href="admin-index.php">Hem</a></li>
+                    <li><a href="admin-news.php">Nyheter</a></li>
+                    <li><a href="admin-sidebar.php">Annonser</a></li>
                     <li><a href="#">Mina hundar</a></li>
                     <li><a href="#">Kennel</a></li>
                     <li><a href="#">Hundskola</a></li>
-                    <li><a href="manage-gallery.php">Bildgalleri</a></li>
+                    <li><a href="admin-gallery.php">Bildgalleri</a></li>
                     <li><a href="admin-thread.php">Gästbok</a></li>
                     <li><a href="#">Kontakt</a></li>
                 </ul>
