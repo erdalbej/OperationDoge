6e768559913f857298ee98c6c8660e98cadded1c
erdalbej <erdbej@gmail.com>
2015-12-28 17:47:26 +0100

    script altered

--- dbScript.sql
+++ dbScript.sql
@@ -89,10 +89,11 @@ CREATE TABLE Puppy (
     `DogName` varchar(255) NOT NULL,
     `Gender` varchar(255) NOT NULL,
     `Price` varchar(255) NOT NULL,
+    `PuppyImagePath` varchar(255),
     `Available` BOOLEAN NOT NULL DEFAULT 1,
     `BirthDate` VARCHAR(45),
     `PuppyLitter_LitterTitle` varchar(255) NOT NULL,
-    PRIMARY KEY (`DogName`),
+    PRIMARY KEY (`DogName`, `PuppyLitter_LitterTitle`),
     FOREIGN KEY (`PuppyLitter_LitterTitle`) REFERENCES PuppyLitter (`LitterTitle`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -122,3 +123,26 @@ AS SELECT
    `DC`.`PriorKnowledge` AS `PriorKnowledge`,
    `DC`.`CourseText` AS `CourseText`,IF((`p`.`NumOfParticipants` > 0),`p`.`NumOfParticipants`,0) AS `Participants`
 FROM (`dogcourse` `DC` left join `participantspercourse` `P` on(((`DC`.`CourseName` = `p`.`DogCourse_CourseName`) and (`DC`.`CourseTeacher` = `p`.`DogCourse_CourseTeacher`) and (`DC`.`CourseDate` = `p`.`DogCourse_CourseDate`)))) where (`DC`.`CourseDate` > now()) order by `DC`.`CourseDate`;
+
+
+/* DATA */
+INSERT INTO `OperationDoge`.`PuppyLitter` (`LitterTitle`, `LiterInfo`) VALUES ('A-Kull', 'None');
+INSERT INTO `OperationDoge`.`PuppyLitter` (`LitterTitle`, `LiterInfo`) VALUES ('B-Kull', 'None');
+INSERT INTO `OperationDoge`.`DogCourse` (`CourseName`, `CourseTeacher`, `CourseDate`, `AgeOfDog`, `Gender`, `PriorKnowledge`, `CourseText`) VALUES ('Kommunikation med hunden', 'Sven-Göran', NOW() + INTERVAL 1 DAY, '2', 'B', 'Inga', 'Det är nödvändigt att du kan kommunicera med din hund, så att ni förstår varandra. Det är du som är den begåvade i detta, även om hunden inte ska underskattas. Det är du som valt hunden - hunden har inte valt dig.');
+INSERT INTO `OperationDoge`.`DogCourse` (`CourseName`, `CourseTeacher`, `CourseDate`, `AgeOfDog`, `Gender`, `PriorKnowledge`, `CourseText`) VALUES ('God relation med din hund', 'Magnus', NOW() + INTERVAL 2 DAY, '3', 'B', 'Inga', 'Om du och din hund tycker om varandra får ni en god relation. Och relationen är grunden för kommunikation. Relationen bygger i hundvärlden på samma premisser som i människovärlden.');
+INSERT INTO `OperationDoge`.`DogCourse` (`CourseName`, `CourseTeacher`, `CourseDate`, `AgeOfDog`, `Gender`, `PriorKnowledge`, `CourseText`) VALUES ('Lydnad för kontroll på hunden', 'Zlatan', NOW() + INTERVAL 3 DAY, '0', 'B', 'Inga', 'När din hund lärt sig ordet och innebörden av "sitt", att den sätter sig omedelbart och sitter kvar tills du säger att den får resa sig, då har du kontroll på din hund i en rad olika situationer.');
+INSERT INTO `OperationDoge`.`DogCourse` (`CourseName`, `CourseTeacher`, `CourseDate`, `AgeOfDog`, `Gender`, `PriorKnowledge`, `CourseText`) VALUES ('Agility', 'Zlatan', NOW() + INTERVAL 4 DAY, '4', 'B', 'Lydnad för kontroll på hunden', 'Agility handlar om att på kortast möjliga tid och utan fel eller vägringar ta sig igenom en hinderbana. Agility är roligt för alla, och när det är som bäst är det också en uppvisning i avancerat samarbete.');
+INSERT INTO `OperationDoge`.`DogCourse` (`CourseName`, `CourseTeacher`, `CourseDate`, `AgeOfDog`, `Gender`, `PriorKnowledge`, `CourseText`) VALUES ('Bruksprov', 'Stefan', NOW()  + INTERVAL 5 DAY, '7', 'B', 'Kommunikation med hunden, god relation med din hund', 'Bruksprov innehåller klassiska arbetsuppgifter för hundar och förare. Du kan träna och tävla i både lydnad och specialgrenarna: spår, sök, rapport, patrull, skydd och IPO.');
+INSERT INTO `OperationDoge`.`DogCourse` (`CourseName`, `CourseTeacher`, `CourseDate`, `AgeOfDog`, `Gender`, `PriorKnowledge`, `CourseText`) VALUES ('Patrullhund', 'Batman', NOW() + INTERVAL 6 DAY, '6', 'B', 'Kommunikation med hunden', 'På uppdrag av Armén/Hemvärnet, Flygvapnet och Marinen kan ni utbilda er inom Brukshundklubben till patrullhund och förare. Du placeras i något av Försvarsmaktens olika förband.');
+
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`,  `PuppyImagePath`, `BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Aeter', 'Hane', '2000', 'noimage.jpg', '2006-05-14', 'A-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Afrodite', 'Tik', '2500', 'noimage.jpg', '2006-05-14  ', 'A-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Aiolos', 'Hane', '2650', 'noimage.jpg', '2006-05-14', 'A-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Ajas', 'Hane', '2650', 'noimage.jpg', '2006-05-14', 'A-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Alectrona', 'Tik', '3000', 'noimage.jpg', '2006-05-14', 'A-Kull');
+
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`,  `PuppyImagePath`, `BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Aeter', 'Hane', '2000', 'noimage.jpg', '2006-05-14', 'B-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Afrodite', 'Tik', '2500', 'noimage.jpg', '2006-05-14  ', 'B-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Aiolos', 'Hane', '2650', 'noimage.jpg', '2006-05-14', 'B-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Ajas', 'Hane', '2650', 'noimage.jpg', '2006-05-14', 'B-Kull');
+INSERT INTO `OperationDoge`.`Puppy` (`DogName`, `Gender`, `Price`, `PuppyImagePath`,`BirthDate`, `PuppyLitter_LitterTitle`) VALUES ('Batuulis Alectrona', 'Tik', '3000', 'noimage.jpg', '2006-05-14', 'B-Kull');
