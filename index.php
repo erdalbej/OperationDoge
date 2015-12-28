ca3f1e02e8e6026984651dad9b12ebb5346d48e8
toaap <mrtoap@hotmail.com>
2015-12-28 13:56:56 +0100

    index fix

--- index.php
+++ index.php
@@ -7,17 +7,25 @@ include_once 'aside.php';
 $divOne = query("SELECT Title, DateTime, NewsText, NewsImagePath FROM News");
 $divData = $divOne['data'];
 foreach($divData as $key => $row){
+
+	if($row['NewsImagePath'] === NULL){
+		$image = "noimage.jpg";
+	}else{
+		$image = $row['NewsImagePath'];
+	}
+
 	echo '<span id="news' . $key . '"></span>';
 	echo '<h2>';
 	echo $row['Title'];
 	echo '</h2>';
-	echo '<img src="uploads/'.$row['NewsImagePath'].'" width="100" height="100" class="floatleft" alt="">';
+	echo '<img class="news-image" src="uploads/'.$image.'" width="100" height="100" class="floatleft" alt="">';
 	echo '<p class="details">';
 	echo $row['NewsText'];
 	echo '</p>';
 	echo '<span>';
 	echo $row['DateTime'];
 	echo '</span>';
+	echo '<br/>';
 	echo '<br class="clearleft">';
 }			
 ?>	
