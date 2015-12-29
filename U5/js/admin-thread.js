f62407ca3729eab6025dfceab6e7f9b010f06b4b
toaap <mrtoap@hotmail.com>
2015-12-29 01:51:47 +0100

    asdpa

--- U5/js/admin-thread.js
+++ U5/js/admin-thread.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
-	$("tbody tr").click(function (event) {    
-		var parent = $(event.target).parent();
+	$("#edit-thread").click(function (event) {    
+		var parent = $(this).parent();
 
 		var data = {};
 		data.threadTitle = $(parent).children().eq(0).text();
 		data.threadCreatedAt = $(parent).children().eq(1).text();
 		data.threadUsername = $(parent).children().eq(2).text();
 		data.threadDescription = $(parent).children().eq(3).text();
+
 
 
 		$("#threadTitle").val(data.threadTitle);
