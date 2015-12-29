80693e2c026423aeae6c7ce2b6869a6f71b97053
toaap <mrtoap@hotmail.com>
2015-12-29 11:34:43 +0100

    admin-post update

--- U5/admin-header.php
+++ U5/admin-header.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
     <script>
     $(document).ready(function(){
