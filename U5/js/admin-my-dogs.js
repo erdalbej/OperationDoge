406e35926cb6558f3de04fbdb91757e220679bff
Robert Barlin <robert.barlin@gmail.com>
2015-12-29 15:09:55 +0100

    fix js for my dogs

--- U5/js/admin-my-dogs.js
+++ U5/js/admin-my-dogs.js
@@ -4,27 +4,31 @@ $(document).ready(function(){
 		var parent = $(event.target).parent();
 
 		var data = {};
-		data.dogName = $(parent).children().eq(0).text();
-		data.officialName = $(parent).children().eq(1).text();
-		data.color = $(parent).children().eq(2).text();
-		data.breader = $(parent).children().eq(3).text();
-		data.weight = $(parent).children().eq(3).text();
-		data.height = $(parent).children().eq(4).text();
-		data.mental = $(parent).children().eq(5).text();
-		data.birthdate = $(parent).children().eq(6).text();
-		data.dogImgPath = $(parent).children().eq(7).text();
-		data.genImgPath = $(parent).children().eq(8).text();
-		data.description = $(parent).children().eq(9).text();
-
-		console.log(data);
+		data.dogName = $(parent).children('.nameTd').text();
+		data.officialName = $(parent).children('.officialNameTd').text();
+		data.color = $(parent).children('.colorTd').text();
+		data.breader = $(parent).children('.breaderId').text();
+		data.weight = $(parent).children('.weightTd').text();
+		data.height = $(parent).children('.heightTd').text();
+		data.mental = $(parent).children('mentalTd').text();
+		data.birthdate = $(parent).children('.birthdateTd').text();
+		data.dogImgPath = $(parent).children('.imageDogTd').text();
+		data.genImgPath = $(parent).children('imageGenTableTd').text();
+		data.description = $(parent).children('.descTd').text();
 
 		$("#edit-dog-name").val(data.dogName);
 		$("#edit-official-name").val(data.officialName);
+		$("#edit-color").val(data.color);
+		$("#edit-breader").val(data.breader);
+		$("#edit-weight").val(data.weight);
+		$("#edit-height").val(data.height);
+		$("#edit-mental").val(data.mental);
+		$("#edit-birthdate").val(data.birthdate);
+		$("#edit-img-dog").val(data.dogImgPath);
+		$("#edit-img-gen-table").val(data.genImgPath);
+		$("#edit-description").val(data.description);
 		
 		$('html, body').animate({ scrollTop: $(document).height() }, 500);
-		$('form').show(300);
-		
-	})
-
-
-})
+		$('form').show(300);	
+	});
+});
