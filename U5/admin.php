eac94539f1d9d74dcd70ac73b6fabecc96903fd4
erdalbej <erdbej@gmail.com>
2015-12-29 21:18:15 +0100

    some more on loginpage

--- U5/admin.php
+++ U5/admin.php
@@ -1,22 +1,24 @@
 <?php
 session_start();
 include_once 'data/dbHelper.php';	
-if ($_SERVER['REQUEST_METHOD'] == "POST" &&
-	isset($_POST['login-submit']) && 
-	isset($_POST['password']) && 
-	isset($_POST['username'])){
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$result = query("SELECT `Username`,`Password` FROM Admin WHERE Username = :username", array(":username" => $username), true);
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['login-submit']) && isset($_POST['password']) && isset($_POST['username'])){
+	if(strlen($_POST['username']) > 0 && strlen($_POST['password']) > 0){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$result = query("SELECT `Username`,`Password` FROM Admin WHERE Username = :username", array(":username" => $username), true);
 
-if ($result['err'] == null){
-	$data = $result['data'];
-	if ($data['Username'] && $data['Username'] == $username && $data['Password'] == $password){		
-		$_SESSION['isAuth'] = true;
-		$_SESSION['username'] = $data['Username'];
-		header('location: /U5/admin-index.php');
-	} else { $error = 'Fel lösenord eller användarnamn'; }
-} else { $error = 'Något gick fel, prova igen'; }
+		if ($result['err'] == null){
+			$data = $result['data'];
+			if ($data['Username'] && $data['Username'] == $username && $data['Password'] == $password){		
+				$_SESSION['isAuth'] = true;
+				$_SESSION['username'] = $data['Username'];
+				header('location: /U5/admin-index.php');
+			} 
+			else { $error = 'Fel användarnamn och/eller lösenord'; }
+		} 
+		else { $error = 'Något gick fel, prova igen'; }
+	}
+	else { $error = 'Var vänlig och fyll i inmatingsfälten'; }
 }
 ?>
 <!DOCTYPE html>
@@ -28,11 +30,6 @@ if ($result['err'] == null){
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
 </head>
 <body>
-	<?php
-		if(isset($error)){
-			echo $error;
-		}
-	?>
 	<section id="login">
 		<form action="" method="POST">		
 			<label for="username">Användarnamn:</label>
@@ -43,6 +40,11 @@ if ($result['err'] == null){
 			<br>
 			<input type="submit" class="login-button button-primary" name="login-submit" value="Logga in">
 		</form>
+		<?php
+		if(isset($error)){
+			echo '<span class="error-message">' . $error . '</span>';
+		}
+		?>
 	</section>
 </body>
 </html>
