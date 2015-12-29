b6d0ea0d94c2abaea897f71ee64731d65828e2ce
erdalbej <erdbej@gmail.com>
2015-12-29 15:04:04 +0100

    more dogs fix

--- U5/dogs.php
+++ U5/dogs.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'header.php';
 include_once 'aside.php';
-$puppys = $_GET['puppylitter'];
+$puppyName = $_GET['puppylitter'];
 ?>
 <main class="height-uv">
 	<div class="container">
@@ -10,12 +10,11 @@ $puppys = $_GET['puppylitter'];
 				<label for="puppylitter">Välj kull</label>
 				<select id="puppylitters" class="u-full-width" name="puppylitter">
 					<?php
-					$puppyData = query("SELECT LitterTitle FROM PuppyLitter");
-					$puppyRes = $puppyData['data'];
+					$puppys = query("SELECT LitterTitle FROM PuppyLitter");
+					$puppyRes = $puppys['data'];
 					foreach($puppyRes as $key => $puppy){
 						echo "<option>";
 						echo $puppy["LitterTitle"];
-						$puppylitter = $puppy["LitterTitle"];
 						echo '</option>';
 					}
 					?>
@@ -32,33 +31,33 @@ $puppys = $_GET['puppylitter'];
 		<div class="row">
 			<div class="twelve columns">
 				<?php
-				if(strlen($puppys) > 0){
-					$divOne = query("SELECT DogName, Gender, Price, PuppyImagePath, Available, BirthDate, PuppyLitter_LitterTitle FROM Puppy WHERE PuppyLitter_LitterTitle = :puppylitter ORDER BY PuppyLitter_LitterTitle",
+				if(strlen($puppyName) > 0){
+					$result = query("SELECT DogName, Gender, Price, PuppyImagePath, Available, BirthDate, PuppyLitter_LitterTitle FROM Puppy WHERE PuppyLitter_LitterTitle = :puppylitter ORDER BY PuppyLitter_LitterTitle",
 						array(
-							':puppylitter' => $puppys
+							':puppylitter' => $puppyName
 							)
 						);
-					$divData = $divOne['data'];
-					if(count($divData) > 0){
-						foreach($divData as $key => $row){
+					$puppysData = $result['data'];
+					if(count($puppysData) > 0){
+						foreach($puppysData as $key => $p){
 
-							if($row['PuppyImagePath'] === NULL){
+							if($p['PuppyImagePath'] === NULL){
 								$image = "noimage.jpg";
 							}else{
-								$image = $row['PuppyImagePath'];
+								$image = $p['PuppyImagePath'];
 							}
 							echo '<span id="dogs' . $key . '"></span>';
 							echo '<h2>';
-							echo $row['DogName'];
+							echo $p['DogName'];
 							echo '</h2>';
 							echo '<img class="dog-image floatleft" src="uploads/'.$image.'" width="100" height="100" alt="">';
 							echo '<p class="details">';
-							echo '<b>Kull: </b>' . $row['PuppyLitter_LitterTitle'] . '<br>';
-							echo '<b>Födelsedatum: </b>' . $row['BirthDate'] . '<br>';
-							echo '<b>Kön: </b>' . $row['Gender'] . '<br>';
-							echo '<b>Pris: </b>' . $row['Price'] . ' kr<br>';
+							echo '<b>Kull: </b>' . $p['PuppyLitter_LitterTitle'] . '<br>';
+							echo '<b>Födelsedatum: </b>' . $p['BirthDate'] . '<br>';
+							echo '<b>Kön: </b>' . $p['Gender'] . '<br>';
+							echo '<b>Pris: </b>' . $p['Price'] . ' kr<br>';
 							echo '<b>Tillgänglig: </b>';
-							if($row['Available'] === "1"){
+							if($p['Available'] === "1"){
 								echo '<span class="dog-available">Ja</span><br>'; 
 							}
 							else {
