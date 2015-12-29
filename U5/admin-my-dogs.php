346c200d01f2c08d6d10c5a0abb4e081059cec6a
Robert Barlin <robert.barlin@gmail.com>
2015-12-29 14:55:45 +0100

    fix

--- U5/admin-my-dogs.php
+++ U5/admin-my-dogs.php
@@ -129,6 +129,33 @@ if ($myDogsResult['err'] == null){
 									echo '<td class="officialNameTd">';
 									echo $d["OfficialName"];
 									echo '</td>';
+									echo '<td hidden class="colorTd">';
+									echo $d["Color"];
+									echo '</td>';
+									echo '<td hidden class="breaderTd">';
+									echo $d["Breader"];
+									echo '</td>';
+									echo '<td hidden class="weightTd">';
+									echo $d["Weight"];
+									echo '</td>';
+									echo '<td hidden class="heightTd">';
+									echo $d["Height"];
+									echo '</td>';
+									echo '<td hidden class="mentalTd">';
+									echo $d["MentalStatus"];
+									echo '</td>';
+									echo '<td hidden class="birthdateTd">';
+									echo $d["Birthdate"];
+									echo '</td>';
+									echo '<td hidden class="imageDogTd">';
+									echo $d["GodImagePath"];
+									echo '</td>';
+									echo '<td hidden class="imageGenTableTd">';
+									echo $d["GenImagePath"];
+									echo '</td>';
+									echo '<td hidden class="descTd">';
+									echo $d["Description"];
+									echo '</td>';
 									echo '</tr>';
 								}
 							}			
@@ -143,53 +170,63 @@ if ($myDogsResult['err'] == null){
 		<br/>
 
 		<form hidden enctype="multipart/form-data" action="" method="post">
-		<div class="row">
-			<div class="twelve columns">
-				<h3>Redigera Hund</h3>
+			<div class="row">
+				<div class="twelve columns">
+					<h3>Redigera Hund</h3>
+				</div>
 			</div>
-		</div>
+			<div class="row">	
+				<div class="six columns">
+					<label for="title">Hundnamn</label>
+					<input disabled maxlength="255" type="text" name="title" class="u-full-width" id="edit-dog-name">
+				</div>
+				<div class="six columns">
+					<label for="title">Officiellt namn</label>
+					<input disabled maxlength="255" type="text" name="title" class="u-full-width" id="edit-official-name">
+				</div>
+			</div>
 			<div class="row">
 				<div class="six columns">
 					<label for="title">Färg</label>
-					<input maxlength="255" type="text" name="title" class="u-full-width">
+					<input maxlength="255" type="text" name="title" class="u-full-width" id="edit-color">
 				</div>
 				<div class="six columns">
 					<label for="title">Uppfödare</label>
-					<input maxlength="255" type="text" name="title" class="u-full-width">
+					<input maxlength="255" type="text" name="title" class="u-full-width" id="edit-breader">
 				</div>
 			</div>
 			<div class="row">
 				<div class="six columns">
 					<label for="title">Vikt i kg</label>
-					<input min="0" step="0.1" value="0" type="number" name="title" class="u-full-width">
+					<input min="0" step="0.1" value="0" type="number" name="title" class="u-full-width" id="edit-weight">
 				</div>
 				<div class="six columns">
 					<label for="title">Mankhöjd i meter</label>
-					<input min="0" step="0.1" type="number" name="title" class="u-full-width">
+					<input min="0" step="0.1" type="number" name="title" class="u-full-width" id="edit-height">
 				</div>
 			</div>
 			<div class="row">
 				<div class="six columns">
 					<label for="title">MentalStatus</label>
-					<input maxlength="255" type="text" name="title" class="u-full-width">
+					<input maxlength="255" type="text" name="title" class="u-full-width" id="edit-mental">
 				</div>
 				<div class="six columns">
 					<label for="title">Födelsedag</label>
-					<input required type="date" name="title" class="u-full-width">
+					<input required type="date" name="title" class="u-full-width" id="edit-birthdate"> 
 				</div>
 			</div>
 			<div class="row">
 				<div class="six columns">
 					<label for="image-path">Bild på hund:</label>
-					<input type="file" name="news-image" accept=".jpg">	
+					<input type="file" name="news-image" accept=".jpg" id="edit-img-dog">	
 				</div>
 				<div class="six columns">
 					<label for="image-path">Stamtavla:</label>
-					<input type="file" name="news-image" accept=".jpg">	
+					<input type="file" name="news-image" accept=".jpg" id="edit-img-gen-table">	
 				</div>
 			</div>
 			<label for="news-text">Beskrivning:</label>
-			<textarea name="news-text" id="" class="u-full-width"></textarea>
+			<textarea name="news-text" id="" class="u-full-width" id="edit-description"></textarea>
 			<div class="row">
 				<div class="six columns">
 					<input value="Uppdatera" name="news-update" type="submit">		
@@ -199,10 +236,8 @@ if ($myDogsResult['err'] == null){
 				</div>
 			</div>
 		</form>
-
 	</div>
 <script src="js/admin-my-dogs.js"></script>
-	</div>
 </main>
 
 <?php
--- U5/js/admin-my-dogs.js
+++ U5/js/admin-my-dogs.js
@@ -4,22 +4,22 @@ $(document).ready(function(){
 		var parent = $(event.target).parent();
 
 		var data = {};
-		data.news_title = $(parent).children().eq(0).text();
-		data.news_datetime = $(parent).children().eq(1).text();
-		data.news_text = $(parent).children().eq(2).text();
-		data.news_image = $(parent).children().eq(3).text();
+		data.dogName = $(parent).children().eq(0).text();
+		data.officialName = $(parent).children().eq(1).text();
+		data.color = $(parent).children().eq(2).text();
+		data.breader = $(parent).children().eq(3).text();
+		data.weight = $(parent).children().eq(3).text();
+		data.height = $(parent).children().eq(4).text();
+		data.mental = $(parent).children().eq(5).text();
+		data.birthdate = $(parent).children().eq(6).text();
+		data.dogImgPath = $(parent).children().eq(7).text();
+		data.genImgPath = $(parent).children().eq(8).text();
+		data.description = $(parent).children().eq(9).text();
 
+		console.log(data);
 
-		$("#edit-title-id").val(data.news_title);
-		$("#edit-date-id").val(data.news_datetime);
-		$("#edit-text-id").val(data.news_text);
-		if(data.news_image !== ""){
-			$("#edit-old-image-id").attr('src', 'uploads/' + data.news_image);
-		}else{
-			$("#edit-old-image-id").attr('src', 'uploads/noimage.jpg');
-		}
-
-		$("#returnMsg").html("");
+		$("#edit-dog-name").val(data.dogName);
+		$("#edit-official-name").val(data.officialName);
 		
 		$('html, body').animate({ scrollTop: $(document).height() }, 500);
 		$('form').show(300);
