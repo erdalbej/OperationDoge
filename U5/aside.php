48a6e616e5176b8e429ac14f89b606dec84f33fc
Robert Barlin <robert.barlin@gmail.com>
2016-01-09 21:42:35 +0100

    stuff

--- U5/aside.php
+++ U5/aside.php
@@ -2,24 +2,30 @@
 	<span class="aside-title">Nyheter</span>
 	<?php
 	$result = query("SELECT NewsTitle, CreatedAt, Description, NewsImagePath, NewsLink FROM NewsFeed ORDER BY CreatedAt DESC LIMIT 3");
-	$newsFeedData = $result['data'];
-	foreach($newsFeedData as $key => $n){
-		echo '<span id="newsfeed' . $key . '"></span>';
-		echo '<div class="sidebar-item">';
-		echo '<div class="news-title">';
-		echo $n['NewsTitle'];
-		echo '</div>';
-		if($n['NewsImagePath'] !== null){
-			echo '<img src="uploads/'.$n['NewsImagePath'].'" class="newsfeed-img" alt="">';
-		}
-		echo '<div class="news-desc">';
-		echo $n['Description'];
-		echo '</div>';
-		if(strlen($n['NewsLink']) > 0){
-			echo '<br><a href="'.$n['NewsLink'].'" class="news-more">Läs mer</a>';
-		}
-		echo '<hr class="newsfeed-sep">';
-		echo '</div>';
-	}			
+
+	if ($result['err'] == null){
+		$newsFeedData = $result['data'];
+		foreach($newsFeedData as $key => $n){
+			echo '<span id="newsfeed' . $key . '"></span>';
+			echo '<div class="sidebar-item">';
+			echo '<div class="news-title">';
+			echo $n['NewsTitle'];
+			echo '</div>';
+			if($n['NewsImagePath'] !== null){
+				echo '<img src="uploads/'.$n['NewsImagePath'].'" class="newsfeed-img" alt="">';
+			}
+			echo '<div class="news-desc">';
+			echo $n['Description'];
+			echo '</div>';
+			if(strlen($n['NewsLink']) > 0){
+				echo '<br><a href="'.$n['NewsLink'].'" class="news-more">Läs mer</a>';
+			}
+			echo '<hr class="newsfeed-sep">';
+			echo '</div>';
+		}		
+	} else {
+		echo '<span>Det gick inte att läsa nyheter, prova ladda om sidan.</span>';
+	}
+		
 	?>	
 </aside>
