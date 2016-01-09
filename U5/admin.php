252991216cd17e39bd76b889ad51ed71d3a04181
Robert Barlin <robert.barlin@gmail.com>
2016-01-09 16:55:53 +0100

    refactor

--- U5/admin.php
+++ U5/admin.php
@@ -3,19 +3,21 @@ session_start();
 include_once 'data/dbHelper.php';	
 if (isset($_POST['login-submit']) && isset($_POST['password']) && isset($_POST['username'])){
 	if(strlen($_POST['username']) > 0 && strlen($_POST['password']) > 0){
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$result = query("SELECT Username, Password FROM Admin WHERE Username = :username", array(":username" => $username), true);
+		if (strlen($_POST['username']) <= 255 && strlen($_POST['password']) <= 255){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$result = query("SELECT Username, Password FROM Admin WHERE Username = :username", array(":username" => $username), true);
 
-		if ($result['err'] == null){
-			$data = $result['data'];
-			if ($data['Username'] == $username && $data['Password'] == $password){		
-				$_SESSION['isAuth'] = true;
-				$_SESSION['username'] = $data['Username'];
-				header('location: /U5/admin-index.php');
-			} 
-			else { $error = 'Fel användarnamn och/eller lösenord'; }
-		} else { $error = 'Något gick fel, prova igen'; }
+			if ($result['err'] == null){
+				$data = $result['data'];
+				if ($data['Username'] == $username && $data['Password'] == $password){		
+					$_SESSION['isAuth'] = true;
+					$_SESSION['username'] = $data['Username'];
+					header('location: /U5/admin-index.php');
+				} 
+				else { $error = 'Fel användarnamn och/eller lösenord'; }
+			} else { $error = 'Något gick fel, prova igen'; }
+		} else { $error = "För stora textvärden, prova igen."; }
 	} else { $error = 'Saknar värden, prova igen!'; }
 }
 ?>
