c604cf9845e55bfcb68932897fec88f57fb5d757
Robert Barlin <robert.barlin@gmail.com>
2016-01-10 13:16:00 +0100

    stuff

--- U5/reset.php
+++ U5/reset.php
@@ -2,6 +2,7 @@
 include_once 'data/dbHelper.php';
 $sqlQueries = fopen("dbScript.sql", "r") or die("Unable to open file!");
 $content = fread($sqlQueries,filesize("dbScript.sql"));
+print_r($content);
 $res = nonQuery($content, array());
 print_r($res);
 ?>
