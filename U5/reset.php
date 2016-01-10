b2264e9beed09a809524f595e9a4895397aadf65
toaap <mrtoap@hotmail.com>
2016-01-10 15:42:00 +0100

    sql

--- U5/reset.php
+++ U5/reset.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'data/dbHelper.php';
-$sqlQueries = fopen("sqlScript", "r") or die("Unable to open file!");
-$content = fread($sqlQueries,filesize("sqlScript"));
+$sqlQueries = fopen("sql/sqlScript", "r") or die("Unable to open file!");
+$content = fread($sqlQueries,filesize("sql/sqlScript"));
 print_r($content);
 $res = nonQuery($content, array());
 print_r($res);
