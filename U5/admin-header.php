264ef75385eecfb9f850f4de1676d99e1203f212
erdalbej <erdbej@gmail.com>
2016-01-10 15:37:01 +0100

    stuff

--- U5/admin-header.php
+++ U5/admin-header.php
@@ -7,7 +7,7 @@
 	<meta charset="UTF-8">
     <meta name="description" content="We are doing operation doge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kennel Batuulis - admin</title>	
+    <title>Kennel Perry - admin</title>	
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
