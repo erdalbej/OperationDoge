c41894667c080ae6431e5805992825c8a1eceb44
erdalbej <erdbej@gmail.com>
2016-01-11 10:29:52 +0100

    stuff

--- U5/gallery.php
+++ U5/gallery.php
@@ -4,21 +4,19 @@ include_once 'aside.php';
 ?>
 <main class="height-uv">
 	<section id="gallery">
-		<p>
 			<?php
 			$result = query("SELECT ImagePath, ImageTitle FROM ImageGallery");
 
 			if ($result['err'] == null){
 				$galleryData = $result['data'];
 				foreach($galleryData as $key => $g){
-					echo '<span id="gallery' . $key . '"></span>';
+					echo '<span id="galleryImg' . $key . '"></span>';
 					echo '<a class="fancybox" href="uploads/'.$g['ImagePath'].'" data-fancybox-group="gallery" title="'.$g['ImageTitle'].'"><img class="gallery-img" src="uploads/'.$g['ImagePath'].'" width="200" height="200" alt="" /></a>';
 				}
 			} else {
 				echo '<span>Det gick inte att läsa in bilder, prova igen.</span>';
 			}		
 			?>
-		</p>
 	</section>
 </main>
 
